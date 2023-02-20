5073ad7604f649686ecf85228035c8be8f5def32
MaksymMohniuk <112825661+MaksymMohniuk@users.noreply.github.com>
2023-02-20 21:48:07 +0200

    update-task-10

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import fetchCountries from './fetchCountries.js';
 import debounce from 'lodash.debounce'
 import Notiflix from 'notiflix';
 
-let country;
+let data;
 const DEBOUNCE_DELAY = 300;
 const searchCountry = document.getElementById('search-box');
 const countryList = document.querySelector('.country-list');
@@ -13,10 +13,29 @@ searchCountry.addEventListener('input', debounce(getCountryName, DEBOUNCE_DELAY)
 
 function getCountryName (event) {
     event.preventDefault();
-    country = event.target.value.trim();
-    return country;
+    data = event.target.value.trim();
+    return data;
 }
 
-// fetchCountries(country).then(data => {
-//       const {name:{official}, capital, population, flags:{svg}, languages} = data[0];
-// })
+fetchCountries(data).then(coutries => {
+    if(coutries.length > 10) {
+        Notify.info("Too many matches found. Please enter a more specific name.");
+    } else if(countries.length >= 2 && countries.length <=10) {
+        const list = countries.reduce((markup, country) => createCountriesList(country) + markup, '');
+        createCountriesList(list);
+    } else {
+        const card = countries.reduce((markup, country) => createCountryCard(country) + markup, '');
+        createCountryCard(card);
+    }
+}
+).catch(onError);
+
+function createCountriesList({flags, name}) {
+    return `<li>
+    <span> </span>
+    </li>`
+}
+
+
+
+    // const {name:{official}, capital, population, flags:{svg}, languages} = data[0];
