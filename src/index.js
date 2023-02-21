c74bb9d43008778876ca46ae1a7ba4618d0a2566
MaksymMohniuk <112825661+MaksymMohniuk@users.noreply.github.com>
2023-02-21 18:35:04 +0200

    update-task-10

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import debounce from 'lodash.debounce'
 import Notiflix from 'notiflix';
 
 let data;
-// const countries;
+let countries;
 const DEBOUNCE_DELAY = 300;
 const searchCountry = document.getElementById('search-box');
 const countryList = document.querySelector('.country-list');
@@ -18,25 +18,37 @@ function getCountryName (event) {
     return data;
 }
 
-// fetchCountries(data).then(coutries => {
-//     if(coutries.length > 10) {
-//         Notify.info("Too many matches found. Please enter a more specific name.");
-//     } else if(countries.length >= 2 && countries.length <=10) {
-//         const list = countries.reduce((markup, country) => createCountriesList(country) + markup, '');
-//         createCountriesList(list);
-//     } else {
-//         const card = countries.reduce((markup, country) => createCountryCard(country) + markup, '');
-//         createCountryCard(card);
-//     }
-// }
-// ).catch(onError);
+fetchCountries(data).then(coutries => {
+    if(coutries.length > 10) {
+        Notify.info("Too many matches found. Please enter a more specific name.");
+    } else if(countries.length >= 2 && countries.length <=10) {
+        const list = countries.reduce((markup, country) => createCountriesList(country) + markup, '');
+        createCountriesList(list);
+    } else {
+        const card = countries.reduce((markup, country) => createCountryCard(country) + markup, '');
+        createCountryCard(card);
+    }
+}
+).catch(onError);
 
-// function createCountriesList({flags, name}) {
-//     return `<li>
-//     <span> </span>
-//     </li>`
-// }
+function createCountriesList({flags, name}) {
+    return `<li>
+    <span>${flags.svg}</span>
+    <p>${name.official}</p>
+    </li>`
+}
 
+function createCountryCard({flags, name, capital, population, languages}) {
+    return  `<span>${flags.svg}</span>
+    <h2>${name.official}</h2>
+    <ul>
+    <li><p>${capital}</p></li>
+    <li><p>${population}</p></li>
+    <li><p>${Object.values(languages)}</p></li>
+    </ul>
+    `
+}
 
-
-    // const {name:{official}, capital, population, flags:{svg}, languages} = data[0];
+function onError(err) {
+    Notify.failure("Oops, there is no country with that name");
+  }
